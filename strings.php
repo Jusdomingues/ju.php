d6db91bd628802ec3252f0cc2fed6e25bbea7fd4
Juliana Domingues <julianasdomingues02@gmail.com>
2022-01-16 22:55:57 -0300

    strings em php e comando quebra de linha

---
+++ strings.php
@@ -0,0 +1,7 @@
+<?php
+
+$idade = 24;
+
+//COMANDO PHP_EOL PARA QUEBRA DE LINHA
+echo "Olá, visitante!" . PHP_EOL;
+echo "Meu nome é Juliana Domingues, tenho $idade anos. Sou uma desenvolvedora back-end curiosa";
